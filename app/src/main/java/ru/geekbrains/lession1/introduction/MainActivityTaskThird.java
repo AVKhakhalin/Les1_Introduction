27e439bdc77f507ef5581d101db3dde78eeb6ecb
Andej Nesterenko <mentatus.n@gmail.com>
2021-07-20 16:59:59 +0300

    стили поправили

--- app/src/main/java/ru/geekbrains/lession1/introduction/MainActivityTaskThird.java
+++ app/src/main/java/ru/geekbrains/lession1/introduction/MainActivityTaskThird.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity_TaskThird extends AppCompatActivity {
+public class MainActivityTaskThird extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
