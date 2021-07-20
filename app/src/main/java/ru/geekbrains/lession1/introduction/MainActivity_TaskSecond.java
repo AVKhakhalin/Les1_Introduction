3e218769944a0f48bb18851cdc0c98a8386a7bfc
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-07-20 13:59:24 +0300

    Выполнен 2 пункт ТЗ:
    Добавлена активити MainActivity_TaskSecond, макет activity_main_task_second.
    В макет activity_main_task_second добавлены элементы EditText со следующими значениями атрибута inputType:
    text, textPersonName, phone, number, textPassword, textEmailAddress, numberPassword, date, datetime, none.
    Исправлен хардкод при выполнении 1 пункта ТЗ.
    
    Ссылка на репозиторий:
    https://github.com/AVKhakhalin/Les1_Introduction

---
+++ app/src/main/java/ru/geekbrains/lession1/introduction/MainActivity_TaskSecond.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lession1.introduction;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity_TaskSecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_task_second);
+    }
+}
